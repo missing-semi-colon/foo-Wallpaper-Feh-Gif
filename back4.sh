273afd82b68a98e1109a6b466924b0c3ba696ef6
missing-semi-colon <67431858+missing-semi-colon@users.noreply.github.com>
2020-12-03 22:01:34 +0000

    Added switch to choose method of running the gif and added more argument validation

--- back4.sh
+++ back4.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
-select1='feh --bg-fill'
-select2='xwallpaper --zoom'
-select3='xloadimage -onroot -fullscreen'
-select4='xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s'
-select5='hsetroot -full'
 
-prog=$select1
-[ "$#" -lt "1" ] || [ "$#" -gt "2" ] && {  echo -e "ERROR : args number invalid \n $0 speed /path/name.gif" ; echo "try 0.010 as speed" ; exit 1 ; }
-dir=/tmp/back4  
-#example of speed  : 0.010
+dir=/tmp/back4
 speed=$1
-name=$2
+gif_path=$2
+prog=$3
 
+[[ "$gif_path" == "" ]] && { gif_path=$speed ; speed=${gif_path##*-} ; }
 
-[[ "$name" == "" ]] && { name=$speed ; speed=${name##*-} ; }
+hash=`md5sum $gif_path | cut -f1 -d" "`
 
-hash=`md5sum $name | cut -f1 -d" "`
+[[ ! -d $dir ]] && mkdir $dir
 
-[[ ! -d $dir ]] && mkdir $dir 
+[[ ! -d $dir/$hash ]] && { mkdir $dir/$hash ; echo "spliting .." ; convert -coalesce $gif_path $dir/$hash/$hash.png ; echo ok ; }
 
-[[ ! -d $dir/$hash ]] && { mkdir $dir/$hash ; echo "spliting .." ; convert -coalesce $name $dir/$hash/$hash.png ; echo ok ; }
-
-while : ; do for i in ` ls $dir/$hash -v ` ; do $prog $dir/$hash/$i ; sleep $speed ; done ; done    
-   
+while : ; do for i in ` ls $dir/$hash -v ` ; do $prog $dir/$hash/$i ; sleep $speed ; done ; done
---
+++ wgif.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+declare -A options=([1]="feh --bg-fill" [2]="xwallpaper --zoom" [3]="xloadimage -onroot -fullscreen" [4]="xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s" [5]="hsetroot -full")
+length=${#options[@]}
+
+help_text="foo-Wallpaper-Feh-Gif (wgif)"
+help_text="${help_text}\nUsage:"
+help_text="${help_text}\n  wgif SPEED GIF_PATH [-t OPTION]"
+help_text="${help_text}\n\nOPTIONs (use the number):"
+for (( i=1; i<${length}+1; i++ ));
+do
+	help_text="${help_text}\n  $i: '${options[$i]}'"
+done
+
+# The speed the gif will play at, 0.01 is often a good value
+speed=$1
+gif_path=$2
+# shift due to the while loop to get the switch options below
+shift; shift
+# Default value for the index selected in the `options` array, can be overriden by passing an argument to the command
+option=1
+# The selected method to play the gif. It is a value chosen from the `options` array above
+prog=
+
+help=false
+while [ "$#" -gt 0 ]; do
+	case $1 in
+		-h|--help) help=true; break ;;
+		-o|--option) option="$2"; shift ;;
+		*) echo "Unknown parameter: $1"; exit 1 ;;
+	esac
+	shift
+done
+if [ "$speed" = "-h" ] || [ "$speed" = "--help" ] || [ "$gif_path" = "-h" ] || [ "$gif_path" = "--help" ]; then
+	help=true
+fi
+if [ $help = true ]; then
+	echo -e "$help_text"
+	exit 0
+fi
+
+number_regex="^[0-9]+([.][0-9]+)?$"
+if [[ ! $speed =~ $number_regex ]]; then
+	echo "'$speed' is not a number"
+	exit 1
+elif [ ! -f "$gif_path" ]; then
+	echo "File '$gif_path' does not exist"
+	exit 1
+fi
+
+prog=${options[$option]}
+
+if [ "$prog" = "" ]; then
+	echo "Invalid value for the 'option' switch";
+	exit 1;
+fi
+
+./back4.sh "$speed" "$gif_path" "$prog"
